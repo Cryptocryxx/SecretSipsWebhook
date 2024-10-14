102009ba3c6157605b12c76ab5eac9f0ebd32026
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-10-14 18:47:17 +0200

    nkm

--- webhook.js
+++ webhook.js
@@ -15,7 +15,7 @@ app.post('/webhook', (req, res) => {
 
   // Führe nur dann git pull aus, wenn die Signatur korrekt ist
   if (req.body.ref === 'refs/heads/main') { // Prüfe, ob der Push auf den 'main'-Branch erfolgt
-    exec('cd /root/SecretSipsBackend && git pull && pm2 restart app.js', (error, stdout, stderr) => {
+    exec('cd /root/SecretSipsBackend && git pull && pm2 restart webhook.js', (error, stdout, stderr) => {
       if (error) {
         console.error(`Fehler bei git pull: ${error}`);
         return res.sendStatus(500); // Fehler beim git pull
