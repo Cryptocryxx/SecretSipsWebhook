ea2d21c23b8022187b17b4fead855452f9ab1591
Cryptocryxx <143177997+Cryptocryxx@users.noreply.github.com>
2024-10-15 09:26:36 +0200

    added npm i to webhook for backend server

--- webhook.js
+++ webhook.js
@@ -14,7 +14,7 @@ app.post('/webhook', (req, res) => {
     }
   // Führe nur dann git pull aus, wenn die Signatur korrekt ist
   if (req.body.ref === 'refs/heads/main') { // Prüfe, ob der Push auf den 'main'-Branch erfolgt
-    exec('cd /home/secret-sips/SecretSipsBackend && git pull && pm2 restart app.js', (error, stdout, stderr) => {
+    exec('cd /home/secret-sips/SecretSipsBackend && git pull && npm i && pm2 restart app.js', (error, stdout, stderr) => {
       if (error) {
         console.error(`Fehler bei git pull: ${error}`);
         return res.sendStatus(500); // Fehler beim git pull
